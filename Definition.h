e3b1fbe75b6aaec7136232fafb257ad77fdebdf3
BuWH <936609271@qq.com>
2017-05-14 23:50:35 +0800

    Add files via upload

--- Definition.h
+++ Definition.h
@@ -5,7 +5,18 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#define MAXWINDOWS 10
+
+#define MAX_WINDOWS 10
+#define OFF_DUTY 0
+#define ON_DUTY 1
+#define WAIT_FOR_QUIT 2
+#define AVAILABLE_PORT 1
+#define CHECKING_PORT 2
+#define SWITCHING_PORT 3
+#define RESTTING_PORT 4
+#define CLOSE_PORT 5
+
+
 struct WindowsPort {
 	int State;
 	int CurNum, CurCustTime;
@@ -15,6 +26,6 @@ struct WindowsPort {
 };
 
 extern int MaxCustSingleLine, MaxLines, MaxSeqLen, MinTimeLen, MaxTimeLen, MinRestSec, MaxRestSec;
-extern struct WindowsPort windows[MAXWINDOWS];
+extern struct WindowsPort windows[MAX_WINDOWS];
 
 #endif
